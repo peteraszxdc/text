f2564de8e8f94583564102f3a8f1f4ba50e7d700
peteraszxdc <peteraszxdc@gmail.com>
2023-10-17 23:42:52 +0800

    m

--- 20231016/20231018HW.py
+++ 20231016/20231018HW.py
@@ -18,9 +18,6 @@ class MyFrame(tk.Frame):
         super().__init__(master, **kwargs)
         self.pack()
 
-        #        scrollbar = ttk.Scrollbar(self, orient="vertical")
-        #        scrollbar.pack(side="right", fill="y")
-
         self.tree = ttk.Treeview(
             self, columns=["#1", "#2", "#3", "#4", "#5", "#6", "#7"], show="headings"
         )
@@ -37,8 +34,48 @@ class MyFrame(tk.Frame):
         for row in price:
             self.tree.insert("", tk.END, value=row)
 
-        self.tree.bind("<<TreeviewSelect>>")
+        self.tree.bind("<<TreeviewSelect>>",self.show_popup)
         deletdata()
+
+    def show_popup(self, event):
+        index = self.tree.selection()[0]
+        tree_dict = self.tree.item(index)
+        values = tree_dict['values']
+        popup = Popup(self, values)
+
+class Popup(Dialog):
+    def __init__(self, master, values):
+        self.values = values
+        super().__init__(master)
+
+    def body(self, master):
+        tk.Label(master, text='日期:').grid(row=0, column=0, sticky='W')
+        tk.Label(master, text='開盤價:').grid(row=1, column=0, sticky='W')
+        tk.Label(master, text='盤中最高價:').grid(row=2, column=0, sticky='W')
+        tk.Label(master, text='盤中最低價:').grid(row=3, column=0, sticky='W')
+        tk.Label(master, text='收盤價:').grid(row=4, column=0, sticky='W')
+        tk.Label(master, text='調整後收盤價:').grid(row=5, column=0, sticky='W')
+        tk.Label(master, text='成交量:').grid(row=6, column=0, sticky='W')
+
+        tk.Label(master, text=self.values[0]).grid(row=0, column=1, sticky='E')
+        tk.Label(master, text=self.values[1]).grid(row=1, column=1, sticky='E')
+        tk.Label(master, text=self.values[2]).grid(row=2, column=1, sticky='E')
+        tk.Label(master, text=self.values[3]).grid(row=3, column=1, sticky='E')
+        tk.Label(master, text=self.values[4]).grid(row=4, column=1, sticky='E')
+        tk.Label(master, text=self.values[5]).grid(row=5, column=1, sticky='E')
+        tk.Label(master, text=self.values[6]).grid(row=6, column=1, sticky='E')
+    def buttonbox(self):
+        box = tk.Frame(self)
+
+        w = tk.Button(box, text="確認", width=10, command=self.ok)
+        w.pack(side='left', padx=5, pady=5)
+        w = tk.Button(box, text="取消", width=10, command=self.cancel)
+        w.pack(side='left', padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
 
 def getdata() -> list[list]:
     data = yf.download("2330.TW", start="2023-01-01")
@@ -48,6 +85,8 @@ def getdata() -> list[list]:
         next(csvReader)
         list_csvReader = [row for row in csvReader]
         return list_csvReader 
+    
+    
 
         
 
